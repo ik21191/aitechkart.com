24ba64c9646cd05961c94dd4c13c9c11b4074004
Imran Khan <imrankhan.in3@gmail.com>
2026-06-02 11:43:35 +0530

    [main]: Updated database configuration

--- php/config.php
+++ php/config.php
@@ -1,5 +1,5 @@
 <?php
 define("DB_HOST", "localhost");
-define("DB_USERNAME", "user");
-define("DB_PASSWORD", "12345678");
-define("DB_DATABASE_NAME", "saiitech");
+define("DB_USERNAME", "saiitech1_saiitech");
+define("DB_PASSWORD", "Saiitech@#000");
+define("DB_DATABASE_NAME", "saiitech1_saiitech");
